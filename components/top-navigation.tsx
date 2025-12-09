3bbcbb0dad4140235aaac03f728352fb87499225
Eugene <eugene.lozon@aix.ph>
2025-12-09 16:33:10 +0800

    update notification badge color

--- components/top-navigation.tsx
+++ components/top-navigation.tsx
@@ -288,7 +288,7 @@ export function TopNavigation() {
                   />
                 </svg>
                 {unreadCount > 0 && (
-                  <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center font-medium">
+                  <span className="absolute -top-1 -right-1 bg-[#1a0f5c] text-white text-xs rounded-full h-5 w-5 flex items-center justify-center font-medium">
                     {unreadCount > 99 ? '99+' : unreadCount}
                   </span>
                 )}
